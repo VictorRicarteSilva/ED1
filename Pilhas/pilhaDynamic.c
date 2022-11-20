b16204e71a5eed1dc2b7f7b2f84e95c7bf7ab464
Victor Ricarte <victorricarte155@gmail.com>
2022-11-20 16:28:53 -0300

    att nas funções criaP, full, empty e push

--- Pilhas/pilhaDynamic.c
+++ Pilhas/pilhaDynamic.c
@@ -10,15 +10,16 @@ typedef struct pilha{ // define o tipo de dado Pilha de tamanho MAX
 
 void criaP(Pilha *p){ // aloca dinamicamente a Pilha na mémoria
     p = malloc(sizeof(Pilha));
+    p->top = 0;
 }
 
 void destroiP(Pilha *p){ // libera a Pilha da mémoria
     free(p);
 }
 
- bool full(Pilha p){ // verifica se a posição no topo e igual ao MAX
+ bool full(Pilha *p){ // verifica se a posição no topo e igual ao MAX
     bool deuCerto;
-    if (p.top == MAX){
+    if (p->top == MAX){
         deuCerto = true;
         return deuCerto;
     }else{
@@ -27,9 +28,9 @@ void destroiP(Pilha *p){ // libera a Pilha da mémoria
     }
 }
 
-bool empty(Pilha p){ // veridica se a posição no topo e igual a 0
+bool empty(Pilha *p){ // veridica se a posição no topo e igual a 0
     bool deuCerto;
-    if(p.top == 0){
+    if(p->top == 0){
         deuCerto = true;
         return deuCerto;
     }else{
@@ -42,12 +43,12 @@ bool empty(Pilha p){ // veridica se a posição no topo e igual a 0
 bool push(Pilha *p, int x){ /* verifica se a Pilha está cheia, caso não incrementa o topo e insere um elemento
                                 na mesma posição*/
     bool deucerto;
-    if(full(*p) == true){
+    if(full(&p) == true){
         deucerto = false;
         return deucerto;
     }else{
-        p->top = p->top + 1;
         p->elem[p->top] = x;
+        p->top = p->top + 1;
         deucerto = true;
         return deucerto;
     }
@@ -56,7 +57,7 @@ bool push(Pilha *p, int x){ /* verifica se a Pilha está cheia, caso não increm
 bool pop(Pilha *p, int *x){ /*verifica se a Pilha está vazia, caso não o ponteiro pra inteiro recebe o elemento
                             do topo e decrementa o topo da Pilha*/
     bool deuCerto;
-    if(empty(*p) == true){
+    if(empty(&p) == true){
         deuCerto = false;
         return deuCerto;
     }else{
@@ -71,11 +72,11 @@ int main(){
     Pilha p1;
     int num;
     criaP(&p1);
-    while(full(p1) == false){
+    while(full(&p1) == false){
         scanf("%d", &num);
         push(&p1, num);
     }
-    while (empty(p1) == false){
+    while (empty(&p1) == false){
         pop(&p1, &num);
         printf("%i", num);
     }
